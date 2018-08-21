b720dccc4d98b4ae2ddd17d2d14e527febd1a312
Dorian Prill <dorian.prill@gmail.com>
2018-08-21 18:47:09 +0200

    fix array bounds for kernel size > 3x3

--- src/convolution.rs
+++ src/convolution.rs
@@ -1,14 +1,15 @@
-// dspr - digital signal processing in rust
-// module: convolution.rs
-// info:   implementations of convolution operations for general 1d and 2d cases
-//         1d is based on Vec<> and 2d is based on ndarray
+//! rustdsp - digital signal processing in rust
+//! module: convolution.rs
+//! info:   implementations of convolution operations for general 1d and 2d cases
+//!         1d is based on Vec<> and 2d is based on ndarray
 
 
 use num_traits::Float;
 use ndarray::prelude::*;
+use std::cmp::max;
 
 
-pub fn convolve(input_a: &Vec<f64>, input_b: &Vec<f64>) -> Vec<f64> {
+pub fn conv(input_a: &Vec<f64>, input_b: &Vec<f64>) -> Vec<f64> {
     let mut vec: Vec<f64> = vec![0f64; (input_a.len() + input_b.len() - 1) as usize];
     for (a_i, &a) in input_a.iter().enumerate() {
         for (b_i, &b) in input_b.iter().enumerate() {
@@ -20,48 +21,51 @@ pub fn convolve(input_a: &Vec<f64>, input_b: &Vec<f64>) -> Vec<f64> {
 
 
 
-#[inline(never)]
-pub fn convolve2d<F>(a: &ArrayView2<F>, b: &ArrayView2<F>, out: &mut ArrayViewMut2<F>)
+//#[inline(never)]
+pub fn conv2d<F>(a: &ArrayView2<F>, b: &ArrayView2<F>, out: &mut ArrayViewMut2<F>)
     where F: Float,
 {
     let (na, ma) = a.dim();
     let (nb, mb) = b.dim();
     let (np, mp) = out.dim();
+    let noff: usize = b.dim().0;
+    let moff: usize = b.dim().1;
     // check if image is smaller than the kernel in any dim
     if na < nb || ma < mb {
         return;
     }
     assert!(np >= na && mp >= ma && np >= nb && mp >= mb);
     // i, j offset by -1 so that we can use unsigned indices
-    unsafe {
-        for i in 0..na - 2 {
-            for j in 0..ma - 2 {
+    //unsafe {
+        for i in 0..na - noff {
+            for j in 0..ma - moff {
                 let mut conv = F::zero();
                 for k in 0..nb {
                     for l in 0..mb {
-                        conv = conv + *a.uget((i + k, j + l)) * *b.uget((k, l));
-                        //conv += a[[i + k, j + l]] * x_kernel[k][l];
+                        //conv = conv + *a.uget((i + k, j + l)) * *b.uget((k, l));
+                        conv = conv + a[[i + k, j + l]] * b[[k, l]];
                     }
                 }
-                *out.uget_mut((i + 1, j + 1)) = conv;
+                //*out.uget_mut((i + 1, j + 1)) = conv;
+                out[[i + 1, j + 1]] = conv;
             }
         }
-    }
+    //}
 }
 
 
 #[cfg(test)]
 mod tests {
     use ndarray::prelude::*;
-    use convolution::convolve;
-    use convolution::convolve2d;
+    use convolution::conv;
+    use convolution::conv2d;
 
     #[test]
     fn conv1d_impulse_response() {
         let x: Vec<f64>   = vec![3.0, 4.0, 5.0]; // excitation signal
         let h: Vec<f64>   = vec![2.0, 1.0, 0.0];    // impulse response of dummy system
         let y: Vec<f64>   = vec![6.0, 11.0, 14.0, 5.0, 0.0]; // system output
-        let res: Vec<f64> = convolve(&x,&h);
+        let res: Vec<f64> = conv(&x,&h);
         // This loop prints: 0 1 2
         for r in &res {
             print!("{} ", r);
@@ -88,7 +92,7 @@ mod tests {
         println!("{:2}", a);
         let mut res = Array::zeros(a.dim());
         for _ in 0..100 {
-            convolve2d(&a.view(), &b.view(), &mut res.view_mut());
+            conv2d(&a.view(), &b.view(), &mut res.view_mut());
         }
         println!("{:2}", res);
         //assert_eq!(a.max(), res.max())
